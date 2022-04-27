7e32c9994e75895844ad840bbd2ac1e1f5701ddb
Austen Kinney <AJKKinney@gmail.com>
2022-04-27 10:02:52 -0700

    Main Menu
    - Added a Main Menu for Launching The Game
    - Added a Loading Screen when Launching The Game

---
+++ SaladChefSim/Assets/MainMenuUI.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using UnityEngine.InputSystem;
+
+public class MainMenuUI : MonoBehaviour
+{
+    [Header("Scene Loading Settings")]
+    public Image loadingPanel;
+    public Slider loadingBar;
+
+    private bool launch = false;
+    AsyncOperation loading;
+
+    // Update is called once per frame
+    void Update()
+    {
+        //loads the game
+        if (launch)
+        {
+            BeginLoad();
+        }
+
+        //loading update loop
+        if (loading != null)
+        {
+            //update loading bar progress
+            float loadingProgress = loading.progress;
+            //add 0.1 to account for scene activation
+            loadingBar.value = loadingProgress + 0.1f;
+
+            //activate scene on keypress
+            if (loading.progress >= 0.9f && Keyboard.current.anyKey.wasPressedThisFrame == true)
+            {
+                loading.allowSceneActivation = true;
+            }
+        }
+    }
+
+    //Launch Game
+    public void StartGame()
+    {
+        launch = true;
+    }
+
+    //starts the async loading operation
+    public void BeginLoad()
+    {
+        loadingPanel.gameObject.SetActive(true);
+        loading = SceneManager.LoadSceneAsync("MainScene");
+
+        //hold the scene until activation
+        loading.allowSceneActivation = false;
+        launch = false;
+    }
+}
